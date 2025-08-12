5a8adb7493be9fafb9d0b3567d9d4bd1aa00a95b
Lean 4 VS Code Extension <>
2025-08-12 00:00:51 -0500

    Initial commit

---
+++ vignettes/Lean standalone project/Lean standalone project.lean
@@ -0,0 +1,3 @@
+-- This module serves as the root of the `«Lean standalone project»` library.
+-- Import modules here that should be built as part of the library.
+import «Lean standalone project».Basic
